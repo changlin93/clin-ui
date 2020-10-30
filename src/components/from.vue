9279e6eeb6ae8c5be7ceb22ff505b7fbec63df6f
DESKTOP-K0BIRC6\Administrator <1452429671@qq.com>
2020-10-30 18:03:16 +0800

    feat: 2020年10月30日18:03:10 新建from组件

---
+++ src/components/from.vue
@@ -0,0 +1,31 @@
+<template>
+    <form action="">
+        <input type="text" :value="msg" />
+        <button type="submit">submit</button>
+    </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    setup(props, { slots, attrs, emit }) {
+        console.log(props);
+        console.log(slots);
+        console.log(attrs);
+        console.log(emit);
+
+        const msg = ref('layer msg!');
+
+        const callback = (a: number): void => console.log(a);
+
+        console.log(callback(2));
+
+        return {
+            msg,
+        };
+    },
+});
+</script>
+
+<style scoped></style>
